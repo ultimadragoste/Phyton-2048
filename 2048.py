13b3a8c19569070cfa714d167cb3acd5bc610806
ultimadragoste <150357124+ultimadragoste@users.noreply.github.com>
2025-11-30 15:32:15 +0700

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -1,4 +1,5 @@
 import random
+import curses
 
 board = [
     [0, 0, 0, 0],
@@ -6,12 +7,6 @@ board = [
     [0, 0, 0, 0],
     [0, 0, 0, 0]
 ]
-
-def print_board():
-    print("==========")
-    for row in board:
-        print(row)
-    print("==========")
 
 def spawn():
     empty = []
@@ -23,52 +18,58 @@ def spawn():
         r, c = random.choice(empty)
         board[r][c] = 2
 
-def merge_row_left(row):
-    new = []
+def shift_left(row):
+    new = [0, 0, 0, 0]
+    idx = 0
     for x in row:
         if x != 0:
-            new.append(x)
+            new[idx] = x
+            idx += 1
+    return new
 
-    merged = []
-    i = 0
-    while i < len(new):
-        if i + 1 < len(new) and new[i] == new[i+1]:
-            merged.append(new[i] * 2)
-            i += 2
-        else:
-            merged.append(new[i])
-            i += 1
-
-    while len(merged) < 4:
-        merged.append(0)
-
-    return merged
+def merge_left(row):
+    for i in range(3):
+        if row[i] != 0 and row[i] == row[i+1]:
+            row[i] *= 2
+            row[i+1] = 0
+    return row
 
 def move_left():
     for i in range(4):
-        board[i] = merge_row_left(board[i])
+        r = shift_left(board[i])
+        r = merge_left(r)
+        r = shift_left(r)
+        board[i] = r
 
 def move_right():
     for i in range(4):
-        board[i].reverse()
-        board[i] = merge_row_left(board[i])
-        board[i].reverse()
+        r = board[i]
+        r.reverse()
+        r = shift_left(r)
+        r = merge_left(r)
+        r = shift_left(r)
+        r.reverse()
+        board[i] = r
 
 def move_up():
     for col in range(4):
-        column = [board[0][col], board[1][col], board[2][col], board[3][col]]
-        merged = merge_row_left(column)
-        for row in range(4):
-            board[row][col] = merged[row]
+        r = [board[x][col] for x in range(4)]
+        r = shift_left(r)
+        r = merge_left(r)
+        r = shift_left(r)
+        for x in range(4):
+            board[x][col] = r[x]
 
 def move_down():
     for col in range(4):
-        column = [board[0][col], board[1][col], board[2][col], board[3][col]]
-        column.reverse()
-        merged = merge_row_left(column)
-        merged.reverse()
-        for row in range(4):
-            board[row][col] = merged[row]
+        r = [board[x][col] for x in range(4)]
+        r.reverse()
+        r = shift_left(r)
+        r = merge_left(r)
+        r = shift_left(r)
+        r.reverse()
+        for x in range(4):
+            board[x][col] = r[x]
 
 def can_move():
     for i in range(4):
@@ -85,36 +86,60 @@ def can_move():
                 return True
     return False
 
+def draw(scr):
+    scr.clear()
+    scr.addstr(0, 0, "2048 Game")
+    scr.addstr(1, 0, "W A S D = move | Q = quit")
 
+    y = 3
+    for i in range(4):
+        line = ""
+        for j in range(4):
+            line += str(board[i][j]).rjust(5)
+        scr.addstr(y, 0, line)
+        y += 1
 
+    scr.refresh()
 
+def main(scr):
+    curses.curs_set(0)
 
-spawn()
-spawn()
-print_board()
+    spawn()
+    spawn()
 
-while True:
-    cmd = input("Move use (W/A/S/D), Q for quit: ").lower()
+    while True:
+        draw(scr)
+        key = scr.getch()
 
-    if cmd == "q":
-        break
-    elif cmd == "a":
-        move_left()
-        spawn()
-    elif cmd == "d":
-        move_right()
-        spawn()
-    elif cmd == "w":
-        move_up()
-        spawn()
-    elif cmd == "s":
-        move_down()
-        spawn()
-    else:
-        print("Wrong Input!")
+        try:
+            c = chr(key).lower()
+        except:
+            continue
 
-    print_board()
+        moved = False
 
-    if not can_move():
-        print("GAME OVER! No More Moves!!!!.")
-        break
+        if c == "q":
+            break
+        elif c == "a":
+            move_left()
+            moved = True
+        elif c == "d":
+            move_right()
+            moved = True
+        elif c == "w":
+            move_up()
+            moved = True
+        elif c == "s":
+            move_down()
+            moved = True
+
+        if moved:
+            spawn()
+            if not can_move():
+                draw(scr)
+                scr.addstr(9, 0, "GAME OVER!!!!! Press any key.")
+                scr.refresh()
+                scr.getch()
+                break
+
+curses.wrapper(main)
